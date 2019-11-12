b2693347682a2333898c5439909ede75ca8c492c
oleksandrbyk <obyk320@gmail.com>
2019-11-13 00:24:52 +0200

    updated style and some typo issues

--- cs/script.js
+++ cs/script.js
@@ -2,27 +2,27 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   try {
     switch (request.method) {
-      // Set select "What is the loan for"
+      // Set select "What is the loan for?"
       case SELECTLOAN:
         selectOption("#loanPurpose", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Rent or own?"
       case SELECTRENTOROWN:
         selectOption("#homeOwnership", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Employment Status"
       case SELECTEMPLOYMENTSTATUS:
         selectOption("#employmentStatus", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Personal Annual Income (before taxes)"
       case SELECTPERSONALANUALINCOME:
         changeInputValue("input[name=annualGrossIncome]", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Current Home Address"
       case SETCURRENTHOMEADDRESS:
         changeInputValue("#google-address-input", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Apt, Suite, Unit, etc."
       case SETADDRESSDETAILED:
         changeInputValue("#aptNumber", request.data);
         break;
@@ -34,23 +34,23 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       case SETLASTNAME:
         changeInputValue("#lastName", request.data);
         break;
-      // Set select "What is the loan for"
+      // Set select "Phone Number"
       case SETPHONENUMBER:
           selectOption("#phone", request.data);
         break;
-      // Set last name
+      // Set the month of birthday
       case SETBIRTHMONTH:
         selectOption("#birthMonth", request.data);
         break;
-      // Set last name
+      // Set the day of birthday
       case SETBIRTHDAY:
         selectOption("#birthDay", request.data);
         break;
-      // Set last name
+      // Set the year of birthday
       case SETBIRTHYEAR:
         selectOption("#birthYear", request.data);
         break;
-      // Set last name
+      // Set email address
       case SETEMAIL:
         changeInputValue("#email", request.data);
         break;
